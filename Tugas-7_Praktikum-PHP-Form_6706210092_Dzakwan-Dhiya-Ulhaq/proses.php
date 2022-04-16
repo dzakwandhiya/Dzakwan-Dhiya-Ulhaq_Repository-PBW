e895288b958f4caf443d15f74e19eb7141cd6463
unknown <dzakwandhiya@student.telkomuniversity.ac.id>
2022-04-16 09:58:00 +0700

    Push tugas Praktikum PHP form

---
+++ Tugas-7_Praktikum-PHP-Form_6706210092_Dzakwan-Dhiya-Ulhaq/proses.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Thanks, <b><?php echo $_POST["name"]; ?></b><br>
+Your email is: <?php echo $_POST["email"]; ?><br>
+Your Website is <?php echo $_POST["website"]; ?><br>
+Comment <?php echo $_POST["comment"]; ?><br>
+You are <?php echo $_POST["gender"]; ?>
+</body>
+</html>
